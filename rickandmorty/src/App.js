8a6be620d6d212876a9b981d2c4dda92450eab4f
Eduardo <70926975+librotero@users.noreply.github.com>
2022-05-27 09:58:22 -0300

    1

--- rickandmorty/src/App.js
+++ rickandmorty/src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './modulos/Header/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
 
 function App() {
   return (
+
     <div className="App">
-      <Header />
+      <h1 className="text-center my-16">
+        Rick & Morty <span className="text-primary">App</span>
+      </h1>
+      <div className="container">
+        <div className="row
+         ">
+
+        </div>
+      </div>
     </div>
   );
 }
